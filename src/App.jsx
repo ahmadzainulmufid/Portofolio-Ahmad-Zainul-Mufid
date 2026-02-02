d1cb50bf83ea9c86189068b115bf421cdcdb3270
Ahmad Zainul Mufid <ahmadzainulmufid12@gmail.com>
2026-02-02 23:08:06 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Home from "./components/Home";
-import { Navbar } from "./components/navbar";
+import { Navbar } from "./components/Navbar";
 import Project from "./components/Project";
 
 export default function App() {
